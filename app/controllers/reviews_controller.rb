f4dfcf87078aa7757b1e22e18ec2e9fa2e1a5bc0
hiro onishi <hiro.taiseiyou@gmail.com>
2018-04-26 17:49:34 +0900

    complete

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,6 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     @review = current_user.reviews.create(review_params)
     redirect_to @review.listing
